4c9048bbc2a1aadc639c15ff2757caafc56e1acf
narmatoff <nss@seoready.ru>
2020-07-09 00:20:07 +0300

    add some desktop pages

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -38,7 +38,7 @@ $( document ).ready(function() {
         $(".opac, .okno#okno4").show();
         $(".okno#okno4").css("top", $(document).scrollTop()+50);
         return false;
-    }, 2000);
+    }, 2000000);
 
 
     let inputs = document.querySelectorAll('.input__file');
@@ -58,11 +58,55 @@ $( document ).ready(function() {
       });
     });
 
+    var $visota;
+    $(".block_vnutr .bl_opis_kurs .full_text").click(function(){
+        $visota = $(".block_vnutr .bl_opis_kurs .full_text p").outerHeight();
+        $(this).css("height", $visota);
+        $(this).addClass("opened");
+        return false;
+    });
 
-    $(".block1 .pol1 .bxslider").bxSlider({
-        touchEnabled: false
+    var $visota2;
+    $(".block_obsu .col2 .razv a").click(function(){
+        $visota2 = $(".block_obsu .col2 .comments .height").outerHeight();
+        $(".block_obsu .col2 .comments").css("height", $visota2);
+        $(".block_obsu .col2 .comments").addClass("opened");
+        $(this).slideUp();
+        return false;
+    });
+
+    var $visota3;
+    $(".block_obuch .col_r .kurs .pol2 .bl_text").click(function(){
+        $visota3 = $(this).find("p").outerHeight();
+        if($(this).hasClass("opened")) {
+            $(this).removeAttr("style");
+        } else {
+            $(this).css("height", $visota3);
+        }
+        $(this).toggleClass("opened");
+        $(this).parent().parent().toggleClass("opened");
+        return false;
+    });
+    $(".block_obuch .col_r .kurs .pol2 .open").click(function(){
+        $(this).parent().find(".bl_text").removeAttr("style");
+        $(this).parent().find(".bl_text").toggleClass("opened");
+        $(this).parent().parent().toggleClass("opened");
+        return false;
+    });
+
+    $(".block_vnutr .zanyatiya .zanyatie .open").click(function(){
+        $(this).parent().toggleClass("opened");
+        return false;
+    });
+
+    $(".block_obuch .bl_polez .bxslider").bxSlider({
+        touchEnabled: false,
+        minSlides: 3,
+        maxSlides: 3,
+        moveSlides: 1,
+        slideWidth: 500
     });
     setInterval(function(){
-        $(".block1 .pol1 .bx-next").click();
+        $(".block_obuch .bl_polez .bx-next").click();
     }, 10000);
 });
